7ef891da67f4f02ee003488750f975c5943c7ec3
Miles Nash <mapnash@gmail.com>
2024-06-24 18:33:47 +0100

    Separate common functions in utils file within er package

--- src/entity_relations/JavaGraph.py
+++ src/entity_relations/JavaGraph.py
@@ -5,30 +5,7 @@ import networkx as nx
 from matplotlib import pyplot as plt
 
 from src.entity_relations.ERClasses import *
-
-
-def add_node_to_dict(node_dict, key, item):
-    if key in node_dict:
-        node_dict[key].append(item)
-    else:
-        node_dict[key] = [item]
-
-
-def find_in_dict(name, name_dict, origin):
-    if name in name_dict:
-        if len(name_dict[name]) == 1:
-            return name_dict[name][0]
-        elif len(name_dict[name]) > 1:
-            package_visible = [entity for entity in name_dict[name] if
-                               entity.package == origin.package]
-            if len(package_visible) == 1:
-                return package_visible[0]
-            elif len(package_visible) > 1:
-                file_visible = [entity for entity in package_visible if
-                                entity.file == origin.file]
-                if len(file_visible) > 0:
-                    return file_visible[0]
-    return None
+from src.entity_relations.utils import add_node_to_dict, find_in_dict
 
 
 def print_graph(entities, relations):
